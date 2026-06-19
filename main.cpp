64aea7af41906e7528447ee60e89169189f453bb
reprenter <repasec@outlook.com>
2026-06-19 15:05:05 +0300

    add main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
+#include "./include/GameManager.h"
+
 #include <iostream>
 
 int main() {
+    try {
+        GameManager game;
+        game.run();
+    } catch (const std::exception& ex) {
+        std::cerr << "Fatal error: " << ex.what() << '\n';
+        return 1;
+    }
     return 0;
 }
